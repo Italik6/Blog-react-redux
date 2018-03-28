dc41d306237eacbd4212977a78450dec558febf3
Michal Turek <michael.turek@rizk.com>
2018-03-28 15:54:47 +0200

    lodash

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render () {
+        return(
+            <div>
+                cos tam
+            </div>
+        )
+    };
+}
+
+export default PostsIndex;
